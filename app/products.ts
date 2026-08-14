e7174a33eb68c5468046d461bdac8a93c96dfdaa
stakenumber5-eng <stakenumber5@gmail.com>
2026-08-14 12:35:58 +0400

    Delete app/products.ts

--- app/products.ts
+++
@@ -1,6 +0,0 @@
-export const products = [
-  {id:1,name:"Frost Dragon",category:"Adopt Me",price:4.99,stock:12,emoji:"🐉"},
-  {id:2,name:"Mega Neon Pet",category:"Adopt Me",price:9.99,stock:6,emoji:"✨"},
-  {id:3,name:"Chroma Knife",category:"MM2",price:6.99,stock:8,emoji:"🔪"},
-  {id:4,name:"1,000 Robux",category:"Robux",price:9.99,stock:"∞",emoji:"🪙"}
-];
